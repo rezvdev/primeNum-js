360a230ac8e27bdd68130d49eba99c2c384e633c
rezvsh <rezvshirdel@gmail.com>
2025-10-27 18:11:51 +0330

    shortened version

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // prime numbers:
 let showPrime = (firstNumber, LastNumber) => {
-    for (let i = firstNumber; i <= LastNumber; i++) {
-        if (getPrime(i)) alert(i);
-    }
+    for (let i = firstNumber; i <= LastNumber; i++) if (getPrime(i)) alert(i);
 }
 let getPrime = n => {
         if(n <= 1) return false;
-        for(let i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i === 0) return false;
-        }
+        for(let i = 2; i <= Math.sqrt(n); i++) if(n % i === 0) return false;
         return true;
 }
 showPrime(+prompt("Enter the first number:"), +prompt("Enter the last number:"));
